5b494b95e9b7fc8a84bc2d7aa0efbceda3d1f9a2
DARRENGOH69 <darrengey-wp20@student.tarc.edu.my>
2024-08-04 11:21:44 +0800

    added css and shit

--- Restaurant/restaurants.php
+++ Restaurant/restaurants.php
@@ -14,22 +14,27 @@
         <h1>Restaurants</h1>
         <section class="articles">
             <?php
-            $sql = "SELECT users.avatar, seller.name, seller.detail, seller.address FROM seller JOIN users ON seller.user_id = users.id;";
+            $sql = "SELECT users.avatar, seller.name, seller.detail, seller.address, seller.id FROM seller JOIN users ON seller.user_id = users.id;";
             $statement = $db->prepare($sql);
             $statement->execute();
             
             while ($row = $statement->fetch()) {
+                $avatar = htmlspecialchars($row['avatar'], ENT_QUOTES, 'UTF-8');
+                $name = htmlspecialchars($row["name"], ENT_QUOTES, 'UTF-8');
+                $detail = htmlspecialchars($row["detail"], ENT_QUOTES, 'UTF-8');
+                $address = htmlspecialchars($row["address"], ENT_QUOTES, 'UTF-8');
+                $id = htmlspecialchars($row["id"], ENT_QUOTES, 'UTF-8');
+                
                 echo "<article>";
                 echo "<div class='article-wrapper'>";
-                echo "<figure>";
-                $avatar = htmlspecialchars($row['avatar'], ENT_QUOTES, 'UTF-8');
+                echo "<figure>"; 
                 echo "<img src='" . $avatar . "' alt='avatar_image' class='avatar_image'/>";
                 echo "</figure>";
                 echo "<div class='article-body'>";
-                echo "<h2>" . htmlspecialchars($row["name"], ENT_QUOTES, 'UTF-8') . "</h2>";
-                echo "<p>" . htmlspecialchars($row["detail"], ENT_QUOTES, 'UTF-8') . "</p>";              
-                echo "<p>Address: " . htmlspecialchars($row["address"], ENT_QUOTES, 'UTF-8') . "</p>";
-                echo "<a href='#' class='read-more'>View Our Menu <span class='sr-only'>about " . htmlspecialchars($row["name"], ENT_QUOTES, 'UTF-8') . "</span>";
+                echo "<h2>" . $name . "</h2>";
+                echo "<p>" . $detail . "</p>";              
+                echo "<p>Address: " . $address . "</p>";
+                echo "<a href='restaurant_plan.php?id=" . $id . "' class='read-more'>Read more <span class='sr-only'>about " . $name . "</span>";
                 echo "<svg xmlns='http://www.w3.org/2000/svg' class='icon' viewBox='0 0 20 20' fill='currentColor'>";
                 echo "<path fill-rule='evenodd' d='M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z' clip-rule='evenodd' />";
                 echo "</svg>";

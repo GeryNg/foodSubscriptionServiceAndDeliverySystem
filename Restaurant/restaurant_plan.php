84149511000bf35883a70367543dfe3b88d01b53
DARRENGOH69 <darrengey-wp20@student.tarc.edu.my>
2024-08-05 22:00:33 +0800

    added something into the ting

--- Restaurant/restaurant_plan.php
+++ Restaurant/restaurant_plan.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Restaurant Plan</title>
-    <link rel="stylesheet" href="../css/restaurant.css">
+    <link rel="stylesheet" href="../css/restaurant_plan.css">
 </head>
 <body>
     <?php include '../partials/headers.php'; ?>
